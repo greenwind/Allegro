c1ff7fd8e208ed7c2b08afdf49bb80ccc65fdea7
Marek Lewandowski <marek.levandovski@gmail.com>
2015-05-15 10:53:19 +0200

    added search item by name method

--- allegro/src/main/java/pl/levandovski/allegro/HomePage.java
+++ allegro/src/main/java/pl/levandovski/allegro/HomePage.java
@@ -1,5 +1,11 @@
 package pl.levandovski.allegro;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Class represents Allegro home page
  * 
@@ -7,5 +13,24 @@ package pl.levandovski.allegro;
  *
  */
 public class HomePage {
-
+	
+	public final static Logger LOGGER = LoggerFactory.getLogger(HomePage.class);
+	
+	private WebDriver driver;
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	/**
+     * Search for item
+     * @param searchString - name of the item
+     * @return SearchPage
+     */
+	public SearchPage searchByName(String searchString) {
+		WebElement searchField = driver.findElement(By.id("main-search-text"));
+		WebElement submitButton = driver.findElement(By.className("sprite search-btn"));
+		searchField.sendKeys(searchString);
+		submitButton.click();
+		return new SearchPage(driver);
+	}
 }
--- allegro/src/main/java/pl/levandovski/allegro/SearchPage.java
+++ allegro/src/main/java/pl/levandovski/allegro/SearchPage.java
@@ -1,5 +1,11 @@
 package pl.levandovski.allegro;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Class represents page with search results
  * 
@@ -7,5 +13,18 @@ package pl.levandovski.allegro;
  *
  */
 public class SearchPage {
-
+	private WebDriver driver;
+	public SearchPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public final static Logger LOGGER = LoggerFactory.getLogger(SearchPage.class);
+	
+	public SearchPage searchByName(String searchString) {
+		WebElement searchField = driver.findElement(By.id("main-search-text"));
+		WebElement submitButton = driver.findElement(By.className("sprite search-btn"));
+		searchField.sendKeys(searchString);
+		submitButton.click();
+		return this;
+	}
 }
